eaae1c9e4b84949dfa65df3490996fb31fa0af4c
DylanLindquist <70230076+DylanLindquist@users.noreply.github.com>
2020-12-03 13:14:24 -0700

    Ignore files

---
+++ ImpactAndSystems/Controller/ImpactAndSystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpactAndSystemsApp.swift
+//  ImpactAndSystems
+//
+//  Created by Lindquist, Dylan on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct ImpactAndSystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
